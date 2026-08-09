f7b394811bd13277bf433a8a12db0effb5b0e534
Shikhar-gif <shikhar0538@gmail.com>
2026-08-09 12:35:13 +0000

    Conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "123"
+
+console.log(typeof score);  //we can write like this
+console.log(typeof(score));  // also we can do like this
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;
+// false => 0
+let isLoggedIn = "hitesh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1- true //0 -> false
+// ""->false
+// "hitesh" -> true
+
+
+
+
+
+
+
+
+
