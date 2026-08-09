8881d684ec127ae131b7638e10c90a90ddef78ff
Shikhar-gif <shikhar0538@gmail.com>
2026-08-09 11:59:06 +0000

    Datatypes and its types

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+console.log(3+3); 
+console.log("Hitesh");
+
+
+// number
+// bigint for big iteger
+// null -> standalone value
+// boolean -> true/ false
+// symbol -> uniqueness
+
+
+let age = 18;
+let isLoggedIn = false;
+let state = null
+
+// object 
+
+console.log(typeof "Shikhar");
+console.log(typeof age);
+console.log(typeof undefined);  //undefined
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
