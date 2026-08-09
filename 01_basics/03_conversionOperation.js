c8037b713db8a7c1c1936fafba35103ddd8d4768
Shikhar-gif <shikhar0538@gmail.com>
2026-08-09 12:51:46 +0000

    Conversions Operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,6 +19,33 @@ console.log(booleanIsLoggedIn);
 // "hitesh" -> true
 
 
+// ************ Operation *************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// +,-,*,**,/,%
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2
+
+console.log(str3);
+console.log("1"+ 2);
+console.log(1+"2");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+ 2
+let gameCounter = 100;
+
+
+
+
 
 
 
