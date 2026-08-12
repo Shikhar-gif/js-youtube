4ddf571fc3f79eb9ea427faa9e5dfa31b22c69a8
Shikhar-gif <shikhar0538@gmail.com>
2026-08-12 03:56:18 +0000

    String and it's operation

---
+++ 01_basics/05_string.js
@@ -0,0 +1,33 @@
+const name = "hitesh";
+const repoCount = 50;
+
+
+console.log(name + repoCount);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String(`hiteshhc`);
+
+console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+
+const newString  = gameName.substring(0,4);
+const newStringOne = "   hitesh   ";
+console.log(newStringOne); 
+// console.log(newStringOne , trime());
+const  url = "https://hitesh.com/hitesh%20choudhary";
+
+console.log(url.replace('%20','-'));
+
+console.log(url.replace(`sundar`));
+
+
+
+
+
